ef13c79d7da8527ca6a8d8de4b3045ee18a120b2
victoremmanuel8 <victor.emmanuel.lima493@gmail.com>
2024-07-30 22:25:54 -0300

    30.07.2024
    base do arquivo vite, com lista do que fazer futuramente

---
+++ prj-react-com-vite/src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1>Oq fazer com o vite outros dias:</h1>
+    <ul>
+      <li>definir a ideia principal do que vai ser esse sistema</li>
+      <li>definir o style principal desse projeto</li>
+      <li>procurar sistemas de rotas, talvez seja a mesma coisa do react padrão</li>
+      <li>planejar como tudo vai se comportar</li>
+      <li>estudar styled componentes pra ficar mais facil de se usar quando necessario</li>
+    </ul>
+    </>
+  )
+}
+
+export default App
