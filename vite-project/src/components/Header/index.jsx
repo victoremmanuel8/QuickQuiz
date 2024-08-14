9d5b10e65e5ee4a0f6c9137c189fd3f197c976a1
victoremmanuel8 <victor.emmanuel.lima493@gmail.com>
2024-08-14 15:59:56 -0300

    14.08.2024

--- vite-project/src/components/Header/index.jsx
+++ vite-project/src/components/Header/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import Logo from '../../../public/vite.svg'
+import Logo from '../../../public/logo.svg'
 
 
 const HeaderStyle = styled.header`
--- vite-project/src/components/Questions/indexQgbQuestions.jsx
+++ vite-project/src/components/Questions/indexQgbQuestions.jsx
@@ -55,3 +55,4 @@ export const questionsQgb = [
     answer: "Suécia",
   }
 ];
+  
